2ea656c672347e94c8ab3b25e4afeb7eeb544755
Flavio Nobre <flavionobre11@gmail.com>
2021-12-18 00:13:21 -0300

    feat: add telegraf and change env

---
+++ telegraf/install.sh
@@ -0,0 +1,4 @@
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo tee /etc/apt/trusted.gpg.d/influxdb.asc >/dev/null
+source /etc/os-release
+echo "deb https://repos.influxdata.com/${ID} ${VERSION_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt-get update && sudo apt-get install telegraf
